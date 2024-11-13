ca2adee3628b25b2c8db4ab7f1446ad9bc7eca1c
若只如初见 <736420574@qq.com>
2024-11-13 20:51:28 +0800

    feat:attach to deepseek

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -21,3 +21,5 @@ elif model.upper() == "XINGZHI":
     from agent.xingzhi_agent import call_with_stream, call_with_messages
 elif model.upper() == "QWEN2":
     from agent.qwen2_agent import call_with_stream, call_with_messages
+elif model.upper() == "DEEPSEEK":
+    from agent.deepseek_agent import call_with_stream, call_with_messages
